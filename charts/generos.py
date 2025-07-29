c6c85a0e63eaa63183e93fe9527a96613f6f2296
Marcos Vinicius Reis <m.vinicius.sr11@gmail.com>
2025-07-29 01:55:14 -0300

    --fix generos mais comuns

--- charts/generos.py
+++ charts/generos.py
@@ -3,13 +3,23 @@ import altair as alt
 
 def grafico_top_generos(df):
     st.subheader("🎭 Top 10 Gêneros Mais Comuns")
-    generos = df['listed_in'].str.split(',').explode().str.strip().value_counts().head(10).reset_index()
+
+    generos = (
+        df['listed_in']
+        .dropna()
+        .str.split(',')
+        .explode()
+        .str.strip()
+        .value_counts()
+        .head(10)
+        .reset_index()
+    )
     generos.columns = ['Gênero', 'Contagem']
 
     chart = alt.Chart(generos).mark_bar().encode(
-        x=alt.X('Gênero:O', sort='-y'),
-        y=alt.Y('Contagem:Q'),
+        x=alt.X('Contagem:Q', title='Número de Títulos'),
+        y=alt.Y('Gênero:N', sort='-x', title='Gênero'),
         tooltip=['Gênero', 'Contagem']
-    ).properties(height=300)
+    ).properties(height=350)
 
     st.altair_chart(chart, use_container_width=True)
